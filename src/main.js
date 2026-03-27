9bfcc1e18ae0d9c15cdc3f078b892298aa58b0b5
edgarsago1 <edgarsago@ciencias.unam.mx>
2026-03-27 14:23:50 -0500

    Feature: Versión Hello-World de la plataforma FrontEnd

---
+++ src/main.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const titulo = document.getElementById('mensaje');
+
+    titulo.textContent = "¡Hola Mundo! El Frontend está vivo!";
+
+    console.log("El repositorio de frontend ha sido inicializado correctamente.");
+});
